7effe6d6783c62169b67ec255bed289fca0f63b9
Luke Thompson <luke.thompson@sydney.edu.au>
2026-04-27 03:19:08 +1000

    Feat: Charts init

--- georax/_geometry/__init__.py
+++ georax/_geometry/__init__.py
@@ -1,4 +1,12 @@
 from .base import LocalChart, Manifold
+from ._charts import (
+    CayleyChart,
+    CongruenceExpChart,
+    CongruencePadeChart,
+    ExpChart,
+    PadeChart,
+    RodriguesChart,
+)
 from .euclidean import Euclidean
 from .spd import SPD
 from .special_orthogonal import SO
@@ -6,6 +14,12 @@ from .special_orthogonal import SO
 __all__ = [
     "Manifold",
     "LocalChart",
+    "CayleyChart",
+    "CongruenceExpChart",
+    "CongruencePadeChart",
+    "ExpChart",
+    "PadeChart",
+    "RodriguesChart",
     "Euclidean",
     "SO",
     "SPD",
