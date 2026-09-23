5c65a341427c7de1585e3c883c00145a5701b8a8
Luke Thompson <luke.thompson@sydney.edu.au>
2026-09-23 22:51:15 +1000

    Export the post-Lie bracket

--- georax/_geometry/__init__.py
+++ georax/_geometry/__init__.py
@@ -1,4 +1,4 @@
-from .base import LocalChart, Manifold
+from .base import LocalChart, Manifold, post_lie_bracket
 from .euclidean import Euclidean
 from .spd import SPD
 from .special_orthogonal import SO
@@ -6,6 +6,7 @@ from .special_orthogonal import SO
 __all__ = [
     "Manifold",
     "LocalChart",
+    "post_lie_bracket",
     "Euclidean",
     "SO",
     "SPD",
